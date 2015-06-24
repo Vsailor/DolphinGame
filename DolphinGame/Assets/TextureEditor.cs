a5e0eee554d7bfb64eed28398db67754a7828280
Ivan <sirius9764@gmail.com>
2015-06-24 14:07:38 +0300

    Not good dolphin annimation

---
+++ DolphinGame/Assets/TextureEditor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets
+{
+    static class TextureEditor
+    {
+        public static List<Sprite> GetSpritesFromAtlas(Texture2D atlas, int spritesAmount, float width, float height, float interval, Vector2 position)
+        {
+            var sprites = new List<Sprite>();
+            for (int i = 0; i < spritesAmount; i++)
+            {
+                sprites.Add(Sprite.Create(atlas, new Rect(interval + i * width, 0, width, height), position));
+            }
+            return sprites;
+        }
+    }
+}
