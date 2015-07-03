bdee9af6e3178c5f5e54e756dc08125473be8393
Ivan <sirius9764@gmail.com>
2015-07-03 19:30:32 +0300

    Added gravity

--- DolphinGame/Assets/Dolphin.cs
+++ DolphinGame/Assets/Dolphin.cs
@@ -2,19 +2,29 @@
 using System.Collections;
 
 public class Dolphin : MonoBehaviour {
-    public float spead = 0f;
-    public float jumpSpead = 8f;
-    public float gravity = 20f;
-    private Vector3 moveDirection = Vector3.zero;
+    public float Spead = 0f;
+    public float StandartGravity = 4f;
+    public float CurrentGravity = 4f;
+
     Quaternion save;
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
         save = this.transform.rotation;
-	}
-	// Update is called once per frame
-	void Update () {
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Spead += 0.1f;
+        }
+    }
+
+    void CheckRotation()
+    {
         var controller = GetComponent<Rigidbody2D>();
-        controller.velocity = new Vector2(spead, controller.velocity.y);
+        controller.velocity = new Vector2(Spead, controller.velocity.y);
         if (Mathf.Abs(this.transform.rotation.z) > 0.4)
         {
             this.transform.rotation = save;
@@ -24,5 +34,22 @@ public class Dolphin : MonoBehaviour {
         {
             this.transform.position = new Vector3(-181.36f, this.transform.position.y, this.transform.position.z);
         }
+    }
+    void MoveDown()
+    {
+        CurrentGravity += 0.1f;
+    }
+	// Update is called once per frame
+	void Update () {
+        CheckRotation();
+        if (Input.GetKey(KeyCode.Space))
+        {
+            MoveDown();
+        }
+        else
+        {
+            CurrentGravity = StandartGravity;
+        }
+        GetComponent<Rigidbody2D>().gravityScale = CurrentGravity;
     }
 }
