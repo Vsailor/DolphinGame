e0df41cb4a29a327f796a3e5e89a82889e1abb83
Ivan <sirius9764@gmail.com>
2015-07-04 22:02:32 +0300

    Done clever rotation

--- DolphinGame/Assets/DolphinScript.cs
+++ DolphinGame/Assets/DolphinScript.cs
@@ -1,20 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DolphinScript : MonoBehaviour {
+public class DolphinScript : MonoBehaviour
+{
 
     public float Spead = 4f;
     public float StandartGravity = 3.5f;
     public float CurrentGravity = 4f;
     public float MinSpead;
     public Vector2 LastPosition;
-    Quaternion save;
+    Quaternion LastRotation;
 
     // Use this for initialization
     void Start()
     {
         LastPosition = this.transform.position;
-        save = this.transform.rotation;
+        LastRotation = this.transform.rotation;
     }
     void OnCollisionStay2D(Collision2D collision)
     {
@@ -22,6 +23,7 @@ public class DolphinScript : MonoBehaviour {
         {
             Spead = MinSpead;
         }
+
         if (Input.GetKey(KeyCode.Space))
         {
             if (LastPosition.y > this.transform.position.y)
@@ -30,7 +32,7 @@ public class DolphinScript : MonoBehaviour {
             }
             else
             {
-                if (Spead-0.4f >= MinSpead)
+                if (Spead - 0.4f >= MinSpead)
                 {
                     Spead -= 0.4f;
                 }
@@ -39,35 +41,48 @@ public class DolphinScript : MonoBehaviour {
         }
         if (LastPosition.y < this.transform.position.y)
         {
-            if (Spead - 0.6f >= MinSpead)
+            if (Spead - 0.4f >= MinSpead)
             {
-                Spead -= 0.6f;
+                Spead -= 0.4f;
             }
             return;
         }
 
-        
+
     }
 
     void CheckRotation()
     {
+
         var controller = GetComponent<Rigidbody2D>();
         controller.velocity = new Vector2(Spead, controller.velocity.y);
-        if (Mathf.Abs(this.transform.rotation.z) > 0.4)
+
+
+
+
+
+
+        if (Mathf.Abs(this.transform.rotation.z) > 0.3)
         {
-            this.transform.rotation = save;
+            this.transform.rotation = LastRotation;
         }
-        save = this.transform.rotation;
+
+        LastRotation = this.transform.rotation;
+
+    }
+    void Teleport()
+    {
         if (this.transform.position.x >= 10.3f)
         {
             this.transform.position = new Vector3(-181.36f, this.transform.position.y, this.transform.position.z);
         }
     }
 
-
     void MoveDown()
     {
         CurrentGravity += 0.07f;
+        this.transform.rotation = new Quaternion(this.transform.rotation.x, this.transform.rotation.y, this.transform.rotation.z + 0.02f, this.transform.rotation.w);
+
     }
     // Update is called once per frame
     void Update()
@@ -79,10 +94,17 @@ public class DolphinScript : MonoBehaviour {
         }
         else
         {
+            if (LastPosition.y < this.transform.position.y && Mathf.Abs(this.transform.rotation.z - 0.08f)<=0.3)
+            {
+                this.transform.rotation = new Quaternion(this.transform.rotation.x, this.transform.rotation.y, this.transform.rotation.z - 0.08f, this.transform.rotation.w);
+            }
+
+            //this.transform.rotation = new Quaternion(this.transform.rotation.x, this.transform.rotation.y, this.transform.rotation.z - 0.1f, this.transform.rotation.w);
             CurrentGravity = StandartGravity;
         }
         GetComponent<Rigidbody2D>().gravityScale = CurrentGravity;
 
         LastPosition = this.transform.position;
+        Teleport();
     }
 }
