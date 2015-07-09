5663447b5e0efc7080db9f96352e0762f4e0083a
Ivan <sirius9764@gmail.com>
2015-07-09 16:10:36 +0300

    Added start screen, main screen

---
+++ DolphinGame/Assets/MainButtonScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainButtonScript : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+    void OnMouseDown()
+    {
+        if (this.name == "PlayButton")
+        {
+            Application.LoadLevel("StartScreen");
+        }
+        if (this.name == "WriteAReviewButton")
+        {
+
+        }
+        if (this.name == "QuitButton")
+        {
+            Application.Quit();
+        }
+    }
+    void Update()
+    {
+
+    }
+}
---
+++ DolphinGame/Assets/MainCameraScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCameraScript : MonoBehaviour {
+    public GameObject Background;
+	// Use this for initialization
+	void Start () {
+
+	}
+    void OnGUI()
+    {
+        var resolution = string.Empty;
+        try
+        {
+            resolution = (Screen.width * 1.0 / Screen.height * 1.0).ToString().Substring(0, 4);
+        }
+        catch (System.ArgumentOutOfRangeException)
+        {
+            resolution = (Screen.width * 1.0 / Screen.height * 1.0).ToString();
+        }
+        if (resolution == "1.70")
+        {
+            Background.transform.localScale = new Vector3(1.44f, 1.27f, 1f);
+        }
+        else if (resolution == "1.78")
+        {
+            Background.transform.localScale = new Vector3(1.51f, 1.27f, 1f);
+        }
+        else if (resolution == "1.66")
+        {
+            Background.transform.localScale = new Vector3(1.41f, 1.27f, 1f);
+        }
+        else if (resolution == "1.50")
+        {
+            Background.transform.localScale = new Vector3(1.27f, 1.27f, 1f);
+        }
+        else if (resolution == "1.60")
+        {
+            Background.transform.localScale = new Vector3(1.35f, 1.27f, 1f);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        //print(Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,Screen.height,0)));
+    }
+}
